95543d83d527f690ad0bd3bc55d3d0d17e3005c1
Ershat12 <ershat200@mail.ru>
2024-04-07 17:58:51 +0600

    5

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,7 +11,6 @@ export const Login = () => {
 
   const [login, { isSuccess: loginIsSuccess }] = useLoginMutation();
 
-
   useEffect(() => {
     if (loginIsSuccess) {
       setEmail("");
@@ -22,7 +21,6 @@ export const Login = () => {
 
   const onLoginSubmit = async (event) => {
     event.preventDefault();
-
 
     try {
       const loginCredentials = { password, email: email.toLowerCase() };
